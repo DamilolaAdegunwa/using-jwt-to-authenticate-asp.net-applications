e860b0466b407c2d32bd6da58b02a96daa323e6e
damilola <damee1993@gmail.com>
2019-11-17 07:30:41 +0100

    Add project files.

---
+++ Jwt/Controllers/JWTController.cs
@@ -0,0 +1,30 @@
+﻿using JwtToken.Model;
+using JwtToken.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtToken.Controllers
+{
+    [Authorize]
+    public class JWTController : ControllerBase
+    {
+        IUserValidationService _uservalidationService;
+
+        public JWTController(IUserValidationService uservalidationService)
+        {
+            this._uservalidationService = uservalidationService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("token")]
+        public IActionResult Authenticate([FromBody]UserEntity userParam)
+        {
+            var user = this._uservalidationService.IsValidate(userParam.Username, userParam.Password);
+            if (user == null)
+            {
+                return BadRequest(new { message = "UserName or Password is invalid" });
+            }
+            return Ok(user);
+        }
+    }
+}
---
+++ Jwt/readme.md
@@ -0,0 +1,7 @@
+﻿#this is a template for doing jwt authentication in asp.net
+
+1) I'd try incorporating it into other asp.net app
+2) See how it works with spa applications
+3) with blazor, pages, console 😊
+4) change configuration and see what happens
+5) experiment with other ways we can store the credentials
